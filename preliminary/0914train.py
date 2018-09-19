4f22ee42cd51089d8265d94d0d88cc990c98a5eb
joey3639670 <joey3639570@gmail.com>
2018-09-19 15:21:02 +0800

    Add comments in 0914train.py. Make data loading as a usable function.

--- preliminary/0914train.py
+++ preliminary/0914train.py
@@ -11,26 +11,32 @@ import data.function as datafunc
 import numpy as np
 
 batch_size = 8
-num_epochs = 100000
+num_epochs = 10000
 
 #data loading
 FILENAME = '/home/cch/Downloads/pytorchworkplace/pytorch/preliminary/0903traindata.csv'
 df, quality, datalist = datafunc.load_data(FILENAME,40)
 quality = np.expand_dims(quality,axis=1)
 
-X = datalist.values
-Y = quality
+def train_data_loading(input,output):
+    #input should be a numpy array, e.g. datalist.values, output should be a 1d array.
+    X = input
+    Y = output
+    #Using Output as our training label
+    encoder = LabelEncoder()
+    encoder.fit(Y)
+    Y = encoder.transform(Y)
+    #Split training set and test set to do training and test together
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+    X_train, Y_train =t.FloatTensor(X_train), t.FloatTensor(Y_train)
+    X_test, Y_test =t.FloatTensor(X_test), t.FloatTensor(Y_test) #Since our output is around 0.3~1.1, it should be float tensor
 
-encoder = LabelEncoder()
-encoder.fit(Y)
-Y = encoder.transform(Y)
+    #If using picture label(one hot style), label should be Long tensor
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
-X_train, Y_train =t.FloatTensor(X_train), t.FloatTensor(Y_train)
-X_test, Y_test =t.FloatTensor(X_test), t.FloatTensor(Y_test)
+    train_dataset = t.utils.data.TensorDataset(X_train,Y_train)
+    test_dataset = t.utils.data.TensorDataset(X_test,Y_test)
 
-train_dataset = t.utils.data.TensorDataset(X_train,Y_train)
-test_dataset = t.utils.data.TensorDataset(X_test,Y_test)
+    return train_dataset,test_dataset
 
 #Model definition
 class Net(nn.Module):
@@ -60,10 +66,10 @@ def train(net, device, train_loader, optimizer, epoch):
     net.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = V(data), V(target)
-        optimizer.zero_grad()
+        optimizer.zero_grad() # Must clear your gradient before training
         target = target.view(batch_size,1)
         output = net(data)
-        loss = F.mse_loss(output, target)
+        loss = F.mse_loss(output, target) #If doing classificaiton, use nll_loss or cross_entropy
         loss.backward()
         optimizer.step()
         if batch_idx % 10 == 0:
@@ -83,7 +89,7 @@ def test(net, device, test_loader):
             test_loss += F.mse_loss(output, target, reduction='sum').item() # sum up batch loss
 
     test_loss /= len(test_loader.dataset)
-    print('\nTest set: Average loss: {:.4f}\n'.format(test_loss))
+    print('Test set: Average loss: {:.4f}\n'.format(test_loss))
 
 def restore_net():
     # restore entire net1 to net2
@@ -92,6 +98,7 @@ def restore_net():
 def main():
     device = t.device("cpu")
     #Data Loader (Input Pipeline)
+    train_dataset,test_dataset = train_data_loading(datalist.values,quality)
     train_loader = t.utils.data.DataLoader(dataset=train_dataset,
                                            batch_size=batch_size,
                                            shuffle=True)
@@ -105,7 +112,7 @@ def main():
     for epoch in range(1, num_epochs + 1):
         train(net, device, train_loader, optimizer, epoch)
         test(net, device, test_loader)
-    torch.save(net, 'net.pkl')
+    torch.save(net, 'test.pkl')
 
 if __name__ == '__main__':
     main()
